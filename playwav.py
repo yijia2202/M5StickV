851ca81e9f2b77306de7177d11f4bed45f5886af
Tracy Yi <50742900+yijia2202@users.noreply.github.com>
2020-04-24 07:41:56 +0800

    Create playwav.py

---
+++ playwav.py
@@ -0,0 +1,29 @@
+from fpioa_manager import *
+from Maix import I2S, GPIO
+import audio
+
+fm.register(board_info.SPK_SD, fm.fpioa.GPIO0)
+spk_sd=GPIO(GPIO.GPIO0, GPIO.OUT)
+spk_sd.value(1)
+fm.register(board_info.SPK_DIN,fm.fpioa.I2S0_OUT_D1)
+fm.register(board_info.SPK_BCLK,fm.fpioa.I2S0_SCLK)
+fm.register(board_info.SPK_LRCLK,fm.fpioa.I2S0_WS)
+wav_dev = I2S(I2S.DEVICE_0)
+
+
+def play_wav(fname):
+    player = audio.Audio(path = fname)
+    player.volume(100)
+    wav_info = player.play_process(wav_dev)
+    wav_dev.channel_config(wav_dev.CHANNEL_1,
+        I2S.TRANSMITTER,resolution = I2S.RESOLUTION_16_BIT,
+        align_mode = I2S.STANDARD_MODE)
+    wav_dev.set_sample_rate(wav_info[1])
+    while True:
+        ret = player.play()
+        if ret == None:
+            break
+        elif ret==0:
+            break
+    player.finish()
+
